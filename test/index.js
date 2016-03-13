6d0cf52f2bc645390d0edef5e59d51c8ff390be6
mjbondra <mjbondra@gmail.com>
2016-03-12 19:26:35 -0800

    update tests to use es6 syntax

--- test/index.js
+++ test/index.js
@@ -1,15 +1,18 @@
+'use strict';
 
-var mongoose = require('mongoose');
-
-var uri = process.env.URI || 'mongodb://127.0.0.1/koa_mongoose_store_test'
-  , uri2 = process.env.URI2 || uri + '_alt';
+const mongoose = require('mongoose');
+const sessionStore = require('./session-store');
+const uri = process.env.URI || 'mongodb://127.0.0.1/koa_mongoose_store_test';
+const uri2 = process.env.URI2 || uri + '_alt';
 
 // create default connection
 mongoose.connect(uri);
 
 // test with different session libraries
-require('./session-store')('koa-session-store', 'KoaSessionStore');
-require('./session-store')('koa-generic-session', 'KoaGenericSession');
+sessionStore('koa-generic-session', 'KoaGenericSession');
+sessionStore('koa-session-store', 'KoaSessionStore');
+
 
 // test with secondary mongoose connection
-require('./session-store')('koa-generic-session', 'KoaGenericSession', mongoose.createConnection(uri2));
+sessionStore('koa-generic-session', 'KoaGenericSession', mongoose.createConnection(uri2));
+sessionStore('koa-session-store', 'KoaSessionStore', mongoose.createConnection(uri2));
--- test/session-store.js
+++ test/session-store.js
@@ -1,128 +1,144 @@
+'use strict';
 
-module.exports = function (sessionLibrary, mongooseModel, connection) {
+module.exports = (sessionLibrary, mongooseModel, connection) => {
+  const app = require('koa')();
+  const MongooseStore = require('../');
+  const route = require('koa-route');
+  const request = require('supertest');
+  const session = require(sessionLibrary);
+  const should = require('should');
 
-  var koa = require('koa')
-    , mongooseStore = require('../')
-    , route = require('koa-route')
-    , request = require('supertest')
-    , session = require(sessionLibrary)
-    , should = require('should');
+  const mongooseOptions = { model: mongooseModel };
 
-  var app = koa();
-
-  var mongooseOptions = { model: mongooseModel };
   if (connection) mongooseOptions.connection = connection;
   else connection = require('mongoose');
 
   app.keys = ['some secret key'];
   app.use(session({
-    store: mongooseStore.create(mongooseOptions)
+    store: new MongooseStore(mongooseOptions)
   }));
 
-  var count = 0;
+  let count = 0;
 
-  app.use(route.get('/create', function *() {
+
+  app.use(route.get('/create', function* () {
     this.session.count = count;
     this.session.count++;
     this.status = 204;
   }));
-  app.use(route.get('/read', function *() {
+
+
+  app.use(route.get('/read', function* () {
     this.status = 204;
   }));
-  app.use(route.get('/update', function *() {
+
+
+  app.use(route.get('/update', function* () {
     this.session.count++;
     this.status = 204;
   }));
-  app.use(route.get('/destroy', function *() {
+
+
+  app.use(route.get('/destroy', function* () {
     this.session = null;
     this.status = 204;
   }));
 
-  var server = app.listen()
-    , agent = request.agent(server)
-    , Session = connection.model(mongooseModel)
-    , sessionId;
 
-  describe('koa-session-mongoose for ' + sessionLibrary + ' using database ' + ( connection.name || connection.connections[0].name ), function () {
-    before(function (done) {
-      Session.remove({}, function (err) {
-        done();
-      });
-    });
+  const server = app.listen();
+  const agent = request.agent(server);
+  const Session = connection.model(mongooseModel);
+  let sessionId;
 
-    describe('Initial state', function () {
-      it('Should not have any session documents', function (done) {
-        Session.findOne({}, function (err, session) {
+
+  describe(`koa-session-mongoose for ${sessionLibrary} using database ${connection.name || connection.connections[0].name}`, () => {
+    before(done => Session.remove({}, () => done()));
+    after(done => Session.remove({}, () => done()));
+
+
+    describe('Initial state', () => {
+      it('Should not have any session documents', done => Session
+        .findOne({}, (err, session) => {
           should.equal(session, null);
           done();
-        });
-      });
+        })
+      );
     });
-    describe('Create', function () {
-      it('Should 204', function (done) {
-        agent
-          .get('/create')
-          .expect(204, done);
-      });
-      it('Should create session document (count value should be 1)', function (done) {
-        Session.findOne({}, function (err, session) {
+
+
+    describe('Create', () => {
+      it('Should 204', done => agent
+        .get('/create')
+        .expect(204, done)
+      );
+
+      it('Should create session document (count value should be 1)', done => Session
+        .findOne({}, (err, session) => {
           sessionId = session.id || null;
           JSON.parse(session.blob).count.should.equal(1);
           done();
-        });
-      });
+        })
+      );
     });
-    describe('Read', function () {
-      it('Should 204', function (done) {
-        agent
-          .get('/read')
-          .expect(204, done);
-      });
-      it('Should not change session document (count value should be 1)', function (done) {
-        Session.findOne({ _id: sessionId }, function (err, session) {
+
+
+    describe('Read', () => {
+      it('Should 204', done => agent
+        .get('/read')
+        .expect(204, done)
+      );
+
+      it('Should not change session document (count value should be 1)', done => Session
+        .findOne({ _id: sessionId }, (err, session) => {
           JSON.parse(session.blob).count.should.equal(1);
           done();
-        });
-      });
+        })
+      );
     });
-    describe('Update', function () {
-      it('Should 204', function (done) {
-        agent
-          .get('/update')
-          .expect(204, done);
-      });
-      it('Should update session document (count value should be 2)', function (done) {
-        Session.findOne({ _id: sessionId }, function (err, session) {
+
+
+    describe('Update', () => {
+      it('Should 204', done => agent
+        .get('/update')
+        .expect(204, done)
+      );
+
+      it('Should update session document (count value should be 2)', done => Session
+        .findOne({ _id: sessionId }, (err, session) => {
           JSON.parse(session.blob).count.should.equal(2);
           done();
-        });
-      });
+        })
+      );
     });
-    describe('Destroy', function () {
-      it('Should 204', function (done) {
-        agent
-          .get('/destroy')
-          .expect(204, done);
-      });
-      it('Should delete session document', function (done) {
-        Session.findOne({ _id: sessionId }, function (err, session) {
+
+
+    describe('Destroy', () => {
+      it('Should 204', done => agent
+        .get('/destroy')
+        .expect(204, done)
+      );
+
+      it('Should delete session document', done => Session
+        .findOne({ _id: sessionId }, (err, session) => {
           should.equal(session, null);
           done();
-        });
-      });
+        })
+      );
     });
-    describe('Read', function () {
-      it('Should 204', function (done) {
-        agent
-          .get('/read')
-          .expect(204, done);
-      });
-      it('Should not find a session document', function (done) {
-        Session.findOne({}, function (err, session) {
+
+
+    describe('Read', () => {
+      it('Should 204', done => agent
+        .get('/read')
+        .expect(204, done)
+      );
+
+      it('Should not find a session document', done => Session
+        .findOne({}, (err, session) => {
           should.equal(session, null);
           done();
-        });
-      });
+        })
+      );
     });
   });
 };
